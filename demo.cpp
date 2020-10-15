3f6aa75a2340ee4f1e48f741fb57b7447d7af6be
Alphaluo-Jonty <xluo023@163.com>
2020-10-15 15:44:21 +0800

    modify again

--- demo.cpp
+++ demo.cpp
@@ -5,13 +5,6 @@
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-    cout << "Hello, C++！！！" << endl;
-    return 0;
-}
-
-=======
     //AString str("Hello, C++");
-    cout << "Hello, C++！！！" << endl;
+    cout << "Hello, C++!!!" << endl;
 };
->>>>>>> 781fcccb1621f545f4f453d5e7e1cc33b234c52c
